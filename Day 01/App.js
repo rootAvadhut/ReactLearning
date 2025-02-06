3012f7bd087d2f70f4bc6a4c2275513f0735c114
Avadhut Shinde <72093748+rootAvadhut@users.noreply.github.com>
2025-02-06 17:53:01 +0530

    first

---
+++ Day 01/App.js
@@ -0,0 +1,35 @@
+// const heading=React.createElement("h1",
+//     {id:"heading",
+//     xyz:"abc"},
+//     "this is react");
+
+/**
+ *<div="parent">
+    <div="child">
+    <h1>i am h1 tag<h1>
+    <div>
+ <div>
+ * 
+ */
+//what is crossorigin
+//
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"i am h1 tag"),
+            React.createElement("h2",{},"i am h2 tag")]
+    ),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1",{},"i am h1 tag"),
+            React.createElement("h2",{},"i am h2 tag")]
+    )
+]
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
